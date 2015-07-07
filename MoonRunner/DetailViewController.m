33abe04c584d7a25a3af1333b3b856512c46e250
Dylan Tan <dylan@misfit.com>
2015-07-07 10:42:43 +0800

    create an empty project

---
+++ MoonRunner/DetailViewController.m
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.m
+//  MoonRunner
+//
+//  Created by 檀振兴 on 7/7/15.
+//  Copyright (c) 2015 檀振兴. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+#pragma mark - Managing the detail item
+
+- (void)setDetailItem:(id)newDetailItem {
+    if (_detailItem != newDetailItem) {
+        _detailItem = newDetailItem;
+            
+        // Update the view.
+        [self configureView];
+    }
+}
+
+- (void)configureView {
+    // Update the user interface for the detail item.
+    if (self.detailItem) {
+        self.detailDescriptionLabel.text = [[self.detailItem valueForKey:@"timeStamp"] description];
+    }
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self configureView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MoonRunner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MoonRunner
+//
+//  Created by 檀振兴 on 7/7/15.
+//  Copyright (c) 2015 檀振兴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
